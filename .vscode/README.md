30b3f40650b5cf10001ec03add0607835a765abb
niuxingjie <niuxingjie@8lab.cn>
2022-09-14 10:47:02 +0800

    feat：django channels tutorial-websocket实现聊天室

---
+++ .vscode/README.md
@@ -0,0 +1,13 @@
+
+# Demo
+[django使用channels实现通信](https://www.jianshu.com/p/e4d1e5e3ad39)
+
+cd 
+
+django-admin startproject channels_demo
+
+python manage.py startapp chat
+
+
+
+
---
+++ channels_demo/chat/routing.py
@@ -0,0 +1,6 @@
+# chat/routing.py
+from django.urls import path, re_path
+from . import consumers
+websocket_urlpatterns = [
+    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+]
